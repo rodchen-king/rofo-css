ded26a3e22134a7b4b7d493e0a84a63bc74d1b16
rodchen <rodchen_king@outlook.com>
2019-09-06 10:12:35 +0800

    1. 修复兼容IE10/IE9遗留问题

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // IE polyfill
+import './polyfill';
 import 'core-js/es';
 import 'mutation-observer';
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
